eb5af5e3d76138740cfc76748efb7b38b3b537e7
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2022-12-30 23:08:56 -0700

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+int birthyear = 2001;
+String birthmonth = "March";
+boolean weekend = false;
+System.out.println(birthyear);
+System.out.println(birthmonth);
+System.out.println(weekend);
+  }
+}
