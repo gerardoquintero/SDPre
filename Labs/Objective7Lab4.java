8e8693901af32217af2180b75dc46c5bb8f87a45
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2022-12-30 23:16:26 -0700

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,19 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 1;
+    int sum = 0;
+    while(count <=20) {
+      sum = count + sum;
+
+      count = count + 1;
+
+
+    }
+    System.out.println(sum);
+    System.out.println("GUACAMOLE, ESO ES!");
+  }
+}
+
+        
+      
+
