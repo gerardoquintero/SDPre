c3b31791332eb0cc9b241206b029de5cef4874ab
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2022-12-30 23:12:11 -0700

    complete objective4lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+	public static void main(String[] args) {
+		int birthYear, age, currentYear;
+
+		birthYear = 0;
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("How old are you?");
+
+
+ 		currentYear = 2022;
+    	age = input.nextInt();
+    	birthYear = currentYear - age;
+
+    	System.out.println("You were born in " + birthYear);
+    	input.close();
+
+
+  }
+}
+
+	
