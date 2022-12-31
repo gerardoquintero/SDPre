87a39fee47ac9dbb6b4586aabdbe56e12b643d41
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2022-12-30 23:13:32 -0700

    complete objective5lab4

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Objective5Lab4 {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int num1;
+
+		System.out.println("Please enter a number");
+		num1 = scanner.nextInt(); 
+
+		if (num1 % 2 == 0) {
+			System.out.println("The number is even");
+			
+		}	
+		else if (num1 % 2 == 1) {
+			System.out.println("The number is odd");	
+		}
+	}
+	
+}
